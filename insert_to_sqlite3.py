fc5241ccd6da1ec766c9906bce354f2243b2834f
shinyaohtani <shinya_ohtani@yahoo.co.jp>
2020-12-27 11:24:24 +0900

    format

--- insert_to_sqlite3.py
+++ insert_to_sqlite3.py
@@ -17,6 +17,7 @@ import unicodedata
 # 13 更新の表示(注6)(「0」は変更なし、「1」は変更あり、「2」廃止(廃止データのみ使用))
 # 14 変更理由(「0」は変更なし、「1」市政・区政・町政・分区・政令指定都市施行、「2」住居表示の実施、「3」区画整理、「4」郵便区調整等、「5」訂正、「6」廃止(廃止データのみ使用))
 
+
 class InsertToSQLite3:
     def make_db(
         ken_all_csv="./assets/KEN_ALL.CSV", postalcode_sqlite3="./postalcode.sqlite3"
@@ -36,13 +37,13 @@ class InsertToSQLite3:
             reader = csv.reader(fp)
             # 一行ずつ処理する
             for row in reader:
-                postc = row[2 ] # 郵便番号
-                prefr = unicodedata.normalize('NFKC', row[3 ]) # 都道府県名(読み),
-                cityr = unicodedata.normalize('NFKC', row[4 ]) # 市区町村名(読み),
-                addrr = unicodedata.normalize('NFKC', row[5 ]) # 町域名(読み),
-                pref = row[6 ] # 都道府県名
-                city = row[7 ] # 市区町村名
-                addr = row[8 ] # 町域名
+                postc = row[2]  # 郵便番号
+                prefr = unicodedata.normalize("NFKC", row[3])  # 都道府県名(読み),
+                cityr = unicodedata.normalize("NFKC", row[4])  # 市区町村名(読み),
+                addrr = unicodedata.normalize("NFKC", row[5])  # 町域名(読み),
+                pref = row[6]  # 都道府県名
+                city = row[7]  # 市区町村名
+                addr = row[8]  # 町域名
                 if addr == "以下に掲載がない場合":
                     addr = ""
                 # SQLiteに追加 --- (*3)
@@ -54,9 +55,3 @@ class InsertToSQLite3:
         # データベースを閉じる --- (*4)
         c.execute("commit")
         conn.close()
-
-
-
-
-
-
